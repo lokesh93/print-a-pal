40208a2c3999a15df8424834bf922fd031965b86
lokesh93 <podipireddy5@gmail.com>
2017-05-09 16:20:29 -0400

    new sign in

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :domain         => 'printapal.com',
     :port           => 587,
-    :user_name      => 'podipireddy5@gmail.com',
-    :password       => '123Podi$',
+    :user_name      => 'printapal24@gmail.com',
+    :password       => 'ca$hmoney',
     :authentication => :plain,
     :enable_starttls_auto => true
 }
