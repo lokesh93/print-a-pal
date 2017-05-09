eb91a6bb95f5eeaa3990ab10f6e28a4290bc8137
lokesh93 <podipireddy5@gmail.com>
2017-05-09 16:25:36 -0400

    delete file

--- print_a_pal/app/controllers/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ActionMailer::Base
-    default :from => "podipireddy5@gmail.com"
-
-	 def registration_confirmation(user)
-	    @user = user
-	    mail(:to => "#{user.name} <#{user.email}>", :subject => "Registration Confirmation")
-	 end
-end
